50ce422f97e1c715366d3e42d14beaf239442af8
Jefferson Estevam <jesbmail@gmail.com>
2019-06-16 16:23:35 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/headerComponent';
+import Main from './pages/main'
+import './styles.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Bitch!</h1>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/headerComponent/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header id="main-header">JS HUNT</header>   
+        )
+    }
+}
+
+export default Header;
